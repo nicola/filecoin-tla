7ea2e8f8f5d5f6af83a475c05bd005745c961ef4
nicola <nicola@users.noreply.github.com>
2020-06-11 12:31:21 +0200

    use script to remove unneeded translation

--- pluscal_clean.sh
+++ pluscal_clean.sh
@@ -8,12 +8,12 @@ IN_TRANSLATION_BLOCK=0
 begin="\\* BEGIN TRANSLATION"
 end="\\* END TRANSLATION"
 
-while IFS='$\n' read -r line; do
-	if [[ "$line" =~ $begin ]]; then
+while IFS=$'\n' read -r line; do
+	if [[ "$line" == "$begin"* ]]; then
 		IN_TRANSLATION_BLOCK=1
     echo "\\* BEGIN TRANSLATION"
     echo ""
-	elif [[ "$line" =~ $end ]]; then
+	elif [[ "$line" == "$end"* ]]; then
 		IN_TRANSLATION_BLOCK=0
     echo "\\* END TRANSLATION"
 	elif [[ $IN_TRANSLATION_BLOCK == 0 ]]; then
